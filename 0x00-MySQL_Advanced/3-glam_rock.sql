0a8fc1e82d29f475713a7915ba475303887240ed
SteveCmd <murimi101@gmail.com>
2024-10-17 20:25:33 +0300

    Refactor query to fix ordering issue in Glam rock bands

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,10 @@
 -- Lists all bands with Glam rock as their main style,
 -- ordered by their longevity
 
-SELECT band_name, (IFNULL(split, `2022`) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
-    style = 'Glam rock'
+    FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY
     lifespan DESC;
