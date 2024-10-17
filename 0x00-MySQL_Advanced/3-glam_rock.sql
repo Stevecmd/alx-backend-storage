afeca261ad76fa76f451c05f261f0abc93767e3b
SteveCmd <murimi101@gmail.com>
2024-10-17 20:24:07 +0300

    made the year a string

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- Lists all bands with Glam rock as their main style,
 -- ordered by their longevity
 
-SELECT band_name, (IFNULL(split, 2022) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, `2022`) - formed) AS lifespan
 FROM
     metal_bands
 WHERE
