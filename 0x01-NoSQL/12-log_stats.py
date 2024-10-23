7ee75b54ea64e2de484ffaf3b0aabcdcf79d3ad6
SteveCmd <murimi101@gmail.com>
2024-10-23 19:45:42 +0300

    Refactor log_stats.py to use f-strings for improved readability and efficiency

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,31 +1,53 @@
 #!/usr/bin/env python3
 """Provides some stats about Nginx logs stored in MongoDB"""
-
 from pymongo import MongoClient
 
 
 def log_stats():
     """Provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+    collection = client.logs.nginx
 
     # Total number of logs
     total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print("{} logs".format(total_logs))
 
     # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print("\tmethod {}: {}".format(method, count))
 
     # Number of documents with method=GET and path=/status
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-    print(f"{status_check} status check")
+    print("{} status check".format(status_check))
+
+    # Get top 10 IPs
+    print("IPs:")
+    pipeline = [
+        {
+            "$group": {
+                "_id": "$ip",
+                "totalRequests": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"totalRequests": -1}
+        },
+        {
+            "$limit": 10
+        }
+    ]
+
+    top_ips = collection.aggregate(pipeline)
+
+    for ip_info in top_ips:
+        ip = ip_info['_id']
+        count = ip_info['totalRequests']
+        print("\t{}: {}".format(ip, count))
 
 
 if __name__ == "__main__":
