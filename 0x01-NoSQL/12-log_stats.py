d78c786e051e6844e4699d2edd1b9c90d866ddaf
SteveCmd <murimi101@gmail.com>
2024-10-26 01:43:01 +0300

    Refactor log_stats() to print total number of logs and methods stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,49 +5,25 @@ from pymongo import MongoClient
 
 def log_stats():
     """Provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient
+    client = MongoClient()
     collection = client.logs.nginx
 
     # Total number of logs
     total_logs = collection.count_documents({})
-    print(f"{total_logs({})} logs")
+    print(f"{total_logs} logs")
 
     # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        print(f"\tmethod {method}: " +
-              f"{total_logs({'method': method})}")
+        count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {count}")
 
     # Number of documents with method=GET and path=/status
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
     print("{} status check".format(status_check))
-
-    # # Get top 10 IPs
-    # print("IPs:")
-    # pipeline = [
-    #     {
-    #         "$group": {
-    #             "_id": "$ip",
-    #             "totalRequests": {"$sum": 1}
-    #         }
-    #     },
-    #     {
-    #         "$sort": {"totalRequests": -1}
-    #     },
-    #     {
-    #         "$limit": 10
-    #     }
-    # ]
-
-    # top_ips = collection.aggregate(pipeline)
-
-    # for ip_info in top_ips:
-    #     ip = ip_info['_id']
-    #     count = ip_info['totalRequests']
-    #     print("\t{}: {}".format(ip, count))
 
 
 if __name__ == "__main__":
