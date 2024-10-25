60cb306993c54dea7c5fce0bcf13ef7de22ddfc8
SteveCmd <murimi101@gmail.com>
2024-10-26 01:54:02 +0300

    Refactor log_stats() to use specific MongoDB connection URL and Status check query,Variable naming and Output format

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,24 +6,24 @@ from pymongo import MongoClient
 def log_stats():
     """Provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    collection = client.logs.nginx
+    nginx_collection = client.logs.nginx
 
     # Total number of logs
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print(f"{nginx_collection.count_documents({})} logs")
 
     # Methods stats
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        count = nginx_collection.count_documents({"method": method})
+        print(f"    method {method}: {count}")
 
-    # Number of documents with method=GET and path=/status
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    # Status check count (documents with method=GET and path=/status)
+    status_checks = nginx_collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+    print(f"{status_checks} status check")
 
 
 if __name__ == "__main__":
