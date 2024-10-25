e039306086511109467e9430c83817f58810699d
SteveCmd <murimi101@gmail.com>
2024-10-26 01:39:34 +0300

    Refactor log_stats() to print total number of logs and methods stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,19 +5,19 @@ from pymongo import MongoClient
 
 def log_stats():
     """Provides some stats about Nginx logs stored in MongoDB"""
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient
     collection = client.logs.nginx
 
     # Total number of logs
     total_logs = collection.count_documents({})
-    print("{} logs".format(total_logs))
+    print(f"{total_logs({})} logs")
 
     # Methods stats
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        count = collection.count_documents({"method": method})
-        print("\tmethod {}: {}".format(method, count))
+        print(f"\tmethod {method}: " +
+              f"{total_logs({'method': method})}")
 
     # Number of documents with method=GET and path=/status
     status_check = collection.count_documents(
@@ -25,29 +25,29 @@ def log_stats():
     )
     print("{} status check".format(status_check))
 
-    # Get top 10 IPs
-    print("IPs:")
-    pipeline = [
-        {
-            "$group": {
-                "_id": "$ip",
-                "totalRequests": {"$sum": 1}
-            }
-        },
-        {
-            "$sort": {"totalRequests": -1}
-        },
-        {
-            "$limit": 10
-        }
-    ]
+    # # Get top 10 IPs
+    # print("IPs:")
+    # pipeline = [
+    #     {
+    #         "$group": {
+    #             "_id": "$ip",
+    #             "totalRequests": {"$sum": 1}
+    #         }
+    #     },
+    #     {
+    #         "$sort": {"totalRequests": -1}
+    #     },
+    #     {
+    #         "$limit": 10
+    #     }
+    # ]
 
-    top_ips = collection.aggregate(pipeline)
+    # top_ips = collection.aggregate(pipeline)
 
-    for ip_info in top_ips:
-        ip = ip_info['_id']
-        count = ip_info['totalRequests']
-        print("\t{}: {}".format(ip, count))
+    # for ip_info in top_ips:
+    #     ip = ip_info['_id']
+    #     count = ip_info['totalRequests']
+    #     print("\t{}: {}".format(ip, count))
 
 
 if __name__ == "__main__":
